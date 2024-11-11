7caee248a46bd336da008b4c20bfc2ca5c040726
Juan Tunubala <juantunubala02@gmail.com>
2024-11-11 15:53:22 -0500

    V 0.2
    -Se corrigen unos errores con las anotaciones @Getter y @Setter en la entidad las cuales no dejaban ni traer ni añadir datos desde la base de datos.

--- src/main/java/co/edu/uniquindio/herbariouq/pmvherbariouq/entities/Especimen.java
+++ src/main/java/co/edu/uniquindio/herbariouq/pmvherbariouq/entities/Especimen.java
@@ -7,8 +7,7 @@ import lombok.Setter;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
+
 public class Especimen {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,7 +21,74 @@ public class Especimen {
     private String detallesEcologicos;
     private String linkFoto;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreCientifico() {
+        return nombreCientifico;
+    }
+
+    public void setNombreCientifico(String nombreCientifico) {
+        this.nombreCientifico = nombreCientifico;
+    }
+
+    public String getRecolector() {
+        return recolector;
+    }
+
+    public void setRecolector(String recolector) {
+        this.recolector = recolector;
+    }
+
+    public String getLugarRecolecta() {
+        return lugarRecolecta;
+    }
+
+    public void setLugarRecolecta(String lugarRecolecta) {
+        this.lugarRecolecta = lugarRecolecta;
+    }
+
+    public LocalDate getFechaRecolecta() {
+        return fechaRecolecta;
+    }
+
+    public void setFechaRecolecta(LocalDate fechaRecolecta) {
+        this.fechaRecolecta = fechaRecolecta;
+    }
+
+    public String getDetallesEcologicos() {
+        return detallesEcologicos;
+    }
+
+    public void setDetallesEcologicos(String detallesEcologicos) {
+        this.detallesEcologicos = detallesEcologicos;
+    }
+
+    public String getLinkFoto() {
+        return linkFoto;
+    }
+
+    public void setLinkFoto(String linkFoto) {
+        this.linkFoto = linkFoto;
+    }
+
+    public Especimen(Long id, String nombreCientifico, String recolector, String lugarRecolecta, LocalDate fechaRecolecta, String detallesEcologicos, String linkFoto) {
+        this.id = id;
+        this.nombreCientifico = nombreCientifico;
+        this.recolector = recolector;
+        this.lugarRecolecta = lugarRecolecta;
+        this.fechaRecolecta = fechaRecolecta;
+        this.detallesEcologicos = detallesEcologicos;
+        this.linkFoto = linkFoto;
+    }
+
+    public Especimen() {
+    }
 }
 
 
